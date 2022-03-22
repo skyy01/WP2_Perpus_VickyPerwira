9bd58b1e146f4cea04be45e099a2c963e42c5b12
skyy01 <vickygrs01@gmail.com>
2022-03-22 20:16:19 +0700

    WP3

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('form_validation', 'session', 'database');
+$autoload['libraries'] = array('form_validation', 'session', 'database',);
 
 /*
 | -------------------------------------------------------------------
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('ModelUser', 'ModelBuku');
+$autoload['model'] = array('ModelUser', 'ModelBuku', 'ModelBooking', 'ModelPinjam');
